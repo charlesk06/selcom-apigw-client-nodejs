52718ea3d556f84860df8085649ab97507c43759
Charles Victor <cvkimaro1@gmail.com>
2023-07-22 21:12:43 +0300

    New Server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const apigwCLient = require('./index.js')
+
+const app = express()
+
+
+
+
+//Controllers
+apigwCLient(app);
+
+app.listen(4000,()=>{
+    console.log('To do App server is running on Port 4000')
+})
