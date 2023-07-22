308a1e87c8ec6c2b0b05321689728d83742d42df
Charles Victor <cvkimaro1@gmail.com>
2023-07-22 21:37:05 +0300

    hhjakj

--- authentication.js
+++ authentication.js
@@ -1,7 +1,7 @@
 
 module.exports(app)=>{
 //import package
-const {apigwClient } = require("selcom-apigw-client");
+const {apigwClient } = require("../selcom-apigw-client");
 // initalize a new apiAccess instace with values of the base url, api key and api secret
 
 const apiKey = '202cb962ac59075b964b07152d234b70';
